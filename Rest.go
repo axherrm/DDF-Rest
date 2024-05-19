61f2df1a09d5ab0eb843837e04e9986d25c37b3d
axherrm <axel-herrmann@gmx.net>
2024-05-19 22:12:40 +0200

    add CORS Headers

--- Rest.go
+++ Rest.go
@@ -19,6 +19,7 @@ var votings = []voting{}
 
 func main() {
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	router.GET("/result", getResult)
 	router.GET("/hello", hello_world)
 	router.POST("/vote", appendVote)
@@ -27,6 +28,18 @@ func main() {
 	router.Run("0.0.0.0:8080")
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+        c.Next()
+	}
+}
 
 func getResult(c *gin.Context) {
 	if finishedVoting {
